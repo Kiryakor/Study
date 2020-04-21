07470f1ed7f8812ce134775239507930a236116a
kiraykorIOS <60466904+kiraykorIOS@users.noreply.github.com>
2020-04-21 16:02:29 +0300

    добавились какие-то работы

--- 4 семестр/Метрология программного обеспечения/tinder/tinder/main.cpp
+++ 4 семестр/Метрология программного обеспечения/tinder/tinder/main.cpp
@@ -1,200 +1,200 @@
-////
-////  main.cpp
-////  tinder
-////
-////  Created by Кирилл on 22.02.2020.
-////  Copyright © 2020 Кирилл. All rights reserved.
-////
 //
-//#include <iostream>
-//#include <fstream>
-//#include <vector>
-//#include <string>
+//  main.cpp
+//  tinder
 //
-//using namespace std;
+//  Created by Кирилл on 22.02.2020.
+//  Copyright © 2020 Кирилл. All rights reserved.
 //
-////MARK: Cтруктуры
-//struct Photo{
-//    string href;
-//    string description;
-//};
-//
-//struct User{
-//    string login;
-//    string password;
-//    vector<string> followers;
-//    vector<Photo> photoVector;
-//};
-//
-////MARK: Class
-//class Instagramm{
-//private:
-//    const string path = "/Users/kirill/Desktop/tinder/tinder/file.txt";
-//    vector<User> dataAllUser;
-//    User activeUser;
-//    //обработка строки из файла
-//    void readDataSupport(string data);
-//    // регистрация
-//    void registration();
-//    // вход
-//    void entrance();
-//    //menu
-//    void menu();
-//    //add image
-//    void addImage();
-//    //finc people
-//    void findPeople();
-//    //просмотр фото
-//    void lookPhotoActiveUser();
-//    //просмотр подписчиков
-//    void lookFollowers();
-//    //редактирование данных своих
-//    void editData();
-//    //редактирование описания у фотографий
-//    void editDescriptionPhoto();
-//    //удаление фотографий
-//    void removePhoto();
-//    //save
-//    void saveAndClose();
-//    //старт
-//    void start();
-//    //info
-//    void info();
-//public:
-//    //считывание данных
-//    void readData();
-//};
-//
-//int main() {
-//    Instagramm insta;
-//    insta.readData();
-//    return 0;
-//}
-//
-////MARK: Считывание данных из файла
-//void Instagramm::readDataSupport(string data){
-//    User user;
-//    string line;
-//    int count = 0;
-//    vector<string> followers;
-//    vector<Photo> photoVector;
-//    Photo photo;
-//    for (int i=0;i<data.length();i++){
-//        if (data[i]!='#' && count==0){
-//            line+=data[i];
-//        }else if (data[i]=='#' && count==0){
-//            user.login = line;
-//            line = "";
-//            count++;
-//        }else if (data[i]!='#' && count==1){
-//            line+=data[i];
-//        }else if (data[i]=='#' && count==1){
-//            user.password = line;
-//            count++;
-//            line = "";
-//        }else if (data[i]!='*' && count==2){
-//            if (data[i]!='!'){
-//                line+=data[i];
-//            }else{
-//                followers.push_back(line);
-//                line = "";
-//            }
-//        }else if (data[i]=='*' && count==2){
-//            count++;
-//            user.followers = followers;
-//            line = "";
-//        }else{
-//            if (data[i]!=' ' && data[i]!='|'){
-//                line+=data[i];
-//            }else if (data[i]==' '){
-//                photo.description = line;
-//                line = "";
-//            }else if (data[i]=='|'){
-//                photo.href = line;
-//                line = "";
-//                photoVector.push_back(photo);
-//            }
-//        }
-//    }
-//    user.photoVector = photoVector;
-//    dataAllUser.push_back(user);
-//}
-//
-//void Instagramm::readData(){
-//    ifstream file(path);
-//    if (!file) {
-//        cout << "файл не найден" << endl;
-//    } else {
-//        string data;
-//        while (getline(file, data)) {
-//            readDataSupport(data);
-//        }
-//        file.close();
-//    }
-//    start();
-//}
-//
-////MARK: Вход в приложение
-//void Instagramm::start(){
-//    int choose;
-//    cout << "Выберите пункт меню " << endl;
-//    cout << "1 - авторизация " << endl;
-//    cout << "2 - регистрация " << endl;
-//    cin >> choose;
-//    switch (choose) {
-//        case 1:
-//            entrance();
-//            break;
-//        case 2:
-//            registration();
-//            break;
-//        default:
-//            cout << "error" << endl;
-//            break;
-//    }
-//}
-//
-//void Instagramm::registration(){
-//    string login, password, password2;
-//    cout << "Введите логин " << endl;
-//    cin >> login;
-//    cout << "Введите пароль " << endl;
-//    cin >> password;
-//    cout << "Введите пароль еще раз " << endl;
-//    cin >> password2;
-//    if (password == password2){
-//        cout << "Вы успешно зарегистрировались " << endl;
-//        User user;
-//        user.login = login;
-//        user.password = password;
-//        dataAllUser.push_back(user);
-//        activeUser = user;
-//        menu();
-//    }else{
-//        cout << "Пароли не совпадают " << endl;
-//    }
-//}
-//
-//void Instagramm::entrance(){
-//    string login, password;
-//    cout << "Введите логин " << endl;
-//    cin >> login;
-//    cout << "Введите пароль " << endl;
-//    cin >> password;
-//
-//    for(int i=0;i<dataAllUser.size();i++){
-//        if (dataAllUser[i].login == login){
-//            if(dataAllUser[i].password == password){
-//                activeUser = dataAllUser[i];
-//                cout << "Вы успешно авторизировались " << endl;
-//                menu();
-//            }else {
-//                cout << "Пароль введен неверно " << endl;
-//            }
-//            break;
-//        }
-//    }
-//}
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+//MARK: Cтруктуры
+struct Photo{
+    string href;
+    string description;
+};
+
+struct User{
+    string login;
+    string password;
+    vector<string> followers;
+    vector<Photo> photoVector;
+};
+
+//MARK: Class
+class Instagramm{
+private:
+    const string path = "/Users/kirill/Desktop/tinder/tinder/file.txt";
+    vector<User> dataAllUser;
+    User activeUser;
+    //обработка строки из файла
+    void readDataSupport(string data);
+    // регистрация
+    void registration();
+    // вход
+    void entrance();
+    //menu
+    void menu();
+    //add image
+    void addImage();
+    //finc people
+    void findPeople();
+    //просмотр фото
+    void lookPhotoActiveUser();
+    //просмотр подписчиков
+    void lookFollowers();
+    //редактирование данных своих
+    void editData();
+    //редактирование описания у фотографий
+    void editDescriptionPhoto();
+    //удаление фотографий
+    void removePhoto();
+    //save
+    void saveAndClose();
+    //старт
+    void start();
+    //info
+    void info();
+public:
+    //считывание данных
+    void readData();
+};
+
+int main() {
+    Instagramm insta;
+    insta.readData();
+    return 0;
+}
+
+//MARK: Считывание данных из файла
+void Instagramm::readDataSupport(string data){
+    User user;
+    string line;
+    int count = 0;
+    vector<string> followers;
+    vector<Photo> photoVector;
+    Photo photo;
+    for (int i=0;i<data.length();i++){
+        if (data[i]!='#' && count==0){
+            line+=data[i];
+        }else if (data[i]=='#' && count==0){
+            user.login = line;
+            line = "";
+            count++;
+        }else if (data[i]!='#' && count==1){
+            line+=data[i];
+        }else if (data[i]=='#' && count==1){
+            user.password = line;
+            count++;
+            line = "";
+        }else if (data[i]!='*' && count==2){
+            if (data[i]!='!'){
+                line+=data[i];
+            }else{
+                followers.push_back(line);
+                line = "";
+            }
+        }else if (data[i]=='*' && count==2){
+            count++;
+            user.followers = followers;
+            line = "";
+        }else{
+            if (data[i]!=' ' && data[i]!='|'){
+                line+=data[i];
+            }else if (data[i]==' '){
+                photo.description = line;
+                line = "";
+            }else if (data[i]=='|'){
+                photo.href = line;
+                line = "";
+                photoVector.push_back(photo);
+            }
+        }
+    }
+    user.photoVector = photoVector;
+    dataAllUser.push_back(user);
+}
+
+void Instagramm::readData(){
+    ifstream file(path);
+    if (!file) {
+        cout << "файл не найден" << endl;
+    } else {
+        string data;
+        while (getline(file, data)) {
+            readDataSupport(data);
+        }
+        file.close();
+    }
+    start();
+}
+
+//MARK: Вход в приложение
+void Instagramm::start(){
+    int choose;
+    cout << "Выберите пункт меню " << endl;
+    cout << "1 - авторизация " << endl;
+    cout << "2 - регистрация " << endl;
+    cin >> choose;
+    switch (choose) {
+        case 1:
+            entrance();
+            break;
+        case 2:
+            registration();
+            break;
+        default:
+            cout << "error" << endl;
+            break;
+    }
+}
+
+void Instagramm::registration(){
+    string login, password, password2;
+    cout << "Введите логин " << endl;
+    cin >> login;
+    cout << "Введите пароль " << endl;
+    cin >> password;
+    cout << "Введите пароль еще раз " << endl;
+    cin >> password2;
+    if (password == password2){
+        cout << "Вы успешно зарегистрировались " << endl;
+        User user;
+        user.login = login;
+        user.password = password;
+        dataAllUser.push_back(user);
+        activeUser = user;
+        menu();
+    }else{
+        cout << "Пароли не совпадают " << endl;
+    }
+}
+
+void Instagramm::entrance(){
+    string login, password;
+    cout << "Введите логин " << endl;
+    cin >> login;
+    cout << "Введите пароль " << endl;
+    cin >> password;
+
+    for(int i=0;i<dataAllUser.size();i++){
+        if (dataAllUser[i].login == login){
+            if(dataAllUser[i].password == password){
+                activeUser = dataAllUser[i];
+                cout << "Вы успешно авторизировались " << endl;
+                menu();
+            }else {
+                cout << "Пароль введен неверно " << endl;
+            }
+            break;
+        }
+    }
+}
 
 //MARK: меню
 void Instagramm::menu(){
