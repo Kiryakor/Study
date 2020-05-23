43103fdfa1bcdd682e26b9c8c26576ed1b55d1d1
kiraykorIOS <60466904+kiraykorIOS@users.noreply.github.com>
2020-05-23 19:34:35 +0300

    решить на чем писать мт3

---
+++ 4 семестр/Методы трансляции/mt3/mt3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  mt3
+//
+//  Created by Кирилл on 23.05.2020.
+//  Copyright © 2020 Кирилл. All rights reserved.
+//
+
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int main() {
+}
