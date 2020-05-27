f892ead3f4f3b082ea18e6580303a93955950722
kiraykorIOS <60466904+kiraykorIOS@users.noreply.github.com>
2020-05-27 15:34:27 +0300

    мт3

---
+++ 4 семестр/Методы трансляции/методы трансляций 3/методы трансляций 3/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  методы трансляций 3
+//
+//  Created by Кирилл on 27.05.2020.
+//  Copyright © 2020 Кирилл. All rights reserved.
+//
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    return 0;
+}
