da3dee21d9e027e18cc5dc9a9476b92c8a22a6b1
kiraykorIOS <60466904+kiraykorIOS@users.noreply.github.com>
2020-06-01 12:08:56 +0300

    rename MT3

--- 4 семестр/Методы трансляции/методы трансляций 3/методы трансляций 3/main.cpp
+++ 4 семестр/Методы трансляции/методы трансляций 3/методы трансляций 3/main.cpp
@@ -13,15 +13,15 @@
 
 using namespace std;
 
-vector<int> stack_digit; //стек символов
-vector<char> stack_operation; //стек операций
+vector<int> symbolVector; //стек символов
+vector<char> operationVector; //стек операций
 
 //MARK: Сheck
 //проверяем подходит ли слово под нашу грматику и заполяем наш стек операций и стек символов
-bool check(string str){
+bool checkData(string str){
     string gramm[] = { "Blanket", "E","TZ","+TZ","FH","*FH","x","5","6","(E)","","" }; // грамматика (вообще "" это степень
-    string table_left = "SEZTHFx56+*()#"; // строки таблицы (терминалы + нетерминалы + символ конца строки)
-    string table_top = "x56+*()#"; // столбцы таблицы (терминалы + символ конца строки)
+    string tableLeft = "SEZTHFx56+*()#"; // строки таблицы (терминалы + нетерминалы + символ конца строки)
+    string tableTop = "x56+*()#"; // столбцы таблицы (терминалы + символ конца строки)
 
     int table[14][8] = {
         // x 5 6 + * ( ) #
@@ -48,11 +48,11 @@ bool check(string str){
         char symb = str.front(); // обращение к 1 элементу строки
         char stack_state = stack.back(); // обращение к последнему элементу строки
 
-        unsigned long int index_top = table_top.find(symb);
-        unsigned long int index_left = table_left.find(stack_state);
-        if (index_top == string::npos) return false; //проверка на несуществующую позицию
+        unsigned long int indeTopTable = tableTop.find(symb);
+        unsigned long int indexLeftTable = tableLeft.find(stack_state);
+        if (indeTopTable == string::npos) return false; //проверка на несуществующую позицию
 
-        int command = table[index_left][index_top];
+        int command = table[indexLeftTable][indeTopTable];
         switch (command){
             case 0:
                 return false; // ошибка
@@ -60,14 +60,14 @@ bool check(string str){
                 return true; // верное
             case 88:{
                 if (symb == '5' || symb == '6')
-                    stack_digit.push_back(symb - '0');
+                    symbolVector.push_back(symb - '0');
                 if (symb == 'x') //вместо x добавляем x
-                    stack_digit.push_back(-1);
+                    symbolVector.push_back(-1);
                 if (symb == '*' || symb == '+' || symb == '(' || symb == ')')
-                    stack_operation.push_back(symb);
+                    operationVector.push_back(symb);
                 str.erase(0, 1); // удаляем 0 элемент строки
                 stack.pop_back();
-                continue; //без него не работает
+                continue;
             }
         }
         string state = gramm[command];
@@ -77,19 +77,19 @@ bool check(string str){
     return false;
 }
 
-//MARK: Преобразования Name
-pair<vector<int>,vector<char>> name(vector<int> digit,vector<char> operation){
+//MARK: Преобразования в строку
+pair<vector<int>,vector<char>> conversionData(vector<int> symbol,vector<char> operation){
     unsigned long int size = operation.size();
     for (auto index=0;index<size;index++){
-        if (digit[index] != -1){ //!= x
+        if (symbol[index] != -1){ //!= x
             if (operation[index] == '+'){
-                digit.erase(digit.begin() + index);
+                symbol.erase(symbol.begin() + index);
                 operation.erase(operation.begin() + index);
                 size = operation.size();
                 index = -1;
-            }else if (operation[index] == '*' && digit[index+1] != -1){
-                digit[index] = digit[index] * digit[index + 1];
-                digit.erase(digit.begin() + index + 1);
+            }else if (operation[index] == '*' && symbol[index+1] != -1){
+                symbol[index] = symbol[index] * symbol[index + 1];
+                symbol.erase(symbol.begin() + index + 1);
                 operation.erase(operation.begin() + index);
                 index = -1;
                 size = operation.size();
@@ -98,43 +98,43 @@ pair<vector<int>,vector<char>> name(vector<int> digit,vector<char> operation){
                 break;
             }
         }else{ //==x
-            if (digit[index] == -1 && operation[index] == '^'){
+            if (symbol[index] == -1 && operation[index] == '^'){
                 index++;
                 continue;
             }else{
-                int count = 1;
+                int countX = 1;
                 for(auto j=index;j<size;j++){
-                    if (digit[j] == -1 && operation[j] == '*' && digit[j+1] == -1){
-                        count++;
+                    if (symbol[j] == -1 && operation[j] == '*' && symbol[j+1] == -1){
+                        countX++;
                         if (j == size - 1){
-                            int count2 = count - 1;
-                            digit.erase(digit.begin() + index + count2);
+                            int count2 = countX - 1;
+                            symbol.erase(symbol.begin() + index + count2);
                             while (count2>0) {
-                                digit.erase(digit.begin() + index + count2 - 1);
+                                symbol.erase(symbol.begin() + index + count2 - 1);
                                 operation.erase(operation.begin() + index + count2 - 1);
                                 count2--;
                             }
-                            digit.insert(digit.begin() + index, count);
+                            symbol.insert(symbol.begin() + index, countX);
                             operation.insert(operation.begin() + index, '*');
-                            digit.insert(digit.begin() + index + 1, -1);
+                            symbol.insert(symbol.begin() + index + 1, -1);
                             operation.insert(operation.begin() + index + 1, '^');
-                            digit.insert(digit.begin() + index + 2, count - 1);
+                            symbol.insert(symbol.begin() + index + 2, countX - 1);
                             size = operation.size();
                             break;
                         }
                     }else{
-                        int count2 = count - 1;
-                        digit.erase(digit.begin() + index + count2);
-                        while (count2!=0) {
-                            digit.erase(digit.begin() + index + count2 - 1);
-                            operation.erase(operation.begin() + index + count2 - 1);
-                            count2--;
+                        int countSup = countX - 1;
+                        symbol.erase(symbol.begin() + index + countSup);
+                        while (countSup!=0) {
+                            symbol.erase(symbol.begin() + index + countSup - 1);
+                            operation.erase(operation.begin() + index + countSup - 1);
+                            countSup--;
                         }
-                        digit.insert(digit.begin() + index, count);
+                        symbol.insert(symbol.begin() + index, countX);
                         operation.insert(operation.begin() + index, '*');
-                        digit.insert(digit.begin() + index + 1, -1);
+                        symbol.insert(symbol.begin() + index + 1, -1);
                         operation.insert(operation.begin() + index + 1, '^');
-                        digit.insert(digit.begin() + index + 2, count - 1);
+                        symbol.insert(symbol.begin() + index + 2, countX - 1);
                         size = operation.size();
                         index = -1;
                         break;
@@ -143,83 +143,89 @@ pair<vector<int>,vector<char>> name(vector<int> digit,vector<char> operation){
             }
         }
     }
-    return pair<vector<int>,vector<char>>(digit,operation);
+    return pair<vector<int>,vector<char>>(symbol,operation);
 }
 
-//MARK: Ans
-string Ans(){
-    unsigned long int s = stack_operation.size();
+//MARK: Преобразовние в стоку
+string conversionDataToString(vector<char> operationVector, vector<int> stackDigit){
+    string returnDataLine = "";
+    string xLine = "x";
+    for (int i=0;i<operationVector.size();i++){
+        if (symbolVector[i] == -1)
+            returnDataLine += xLine;
+        else
+            returnDataLine += to_string(symbolVector[i]);
+        returnDataLine += operationVector[i]; //в теории тут можно проверку добавить на digite i+1 != числу
+    }
+    if (symbolVector[symbolVector.size()-1] == -1)
+        returnDataLine += "1";
+    else
+        returnDataLine += to_string(symbolVector[symbolVector.size()-1]);
+    
+    return returnDataLine;
+}
+
+//MARK: Разделение даты на подчасти
+string separationData(){
+    unsigned long int s = operationVector.size();
     bool check = false;
+    //стеки для обработки строк между ()
     vector<int> stackDigit; //стек символов
     vector<char> stackOperation; //стек операций
     int index = 0;
     //избавляемся от скобок
     for (int i=0;i<s;i++){
-        if (stack_operation[i] == '('){
+        if (operationVector[i] == '('){
             index = i;
             check = true;
             stackDigit.clear();
             stackOperation.clear();
-            stackDigit.push_back(stack_digit[i]);
-        }else if (stack_operation[i] == ')'){
+            stackDigit.push_back(symbolVector[i]);
+        }else if (operationVector[i] == ')'){
             int data = 1;
             bool checkData = false;
-            if (index != 0 && stack_operation[index-1] == '*'){
-                data = stack_digit[index - 1];
+            if (index != 0 && operationVector[index-1] == '*'){
+                data = symbolVector[index - 1];
                 index--;
                 checkData = true;
             }
             //получаем пару значение
-            auto p = name(stackDigit,stackOperation); //MARK: умножение на скобку
+            auto p = conversionData(stackDigit,stackOperation);
             //удаляем все значения из скобок в исходном векторе
-            stack_digit.erase(stack_digit.begin()+index,stack_digit.begin()+i);
-            stack_operation.erase(stack_operation.begin()+index,stack_operation.begin()+i+1);
+            symbolVector.erase(symbolVector.begin()+index,symbolVector.begin()+i);
+            operationVector.erase(operationVector.begin()+index,operationVector.begin()+i+1);
             //добавляем преобразованные данные
             for (int i=0; i<p.first.size(); i++){
                 if (checkData){
-                    stack_digit.insert(stack_digit.begin()+index+i, p.first[i]*data);
+                    symbolVector.insert(symbolVector.begin()+index+i, p.first[i]*data);
                     checkData = false;
                 }else{
-                    stack_digit.insert(stack_digit.begin()+index+i, p.first[i]);
+                    symbolVector.insert(symbolVector.begin()+index+i, p.first[i]);
                     if (p.second[i] == '+')
                         checkData = true;
                 }
             }
             for (int i=0; i<p.second.size(); i++){
-                stack_operation.insert(stack_operation.begin()+index+i, p.second[i]);
+                operationVector.insert(operationVector.begin()+index+i, p.second[i]);
             }            i = -1;
-            s = stack_operation.size();
+            s = operationVector.size();
             check = false;
         }else if (check){
-            stackDigit.push_back(stack_digit[i]);
-            stackOperation.push_back(stack_operation[i]);
+            stackDigit.push_back(symbolVector[i]);
+            stackOperation.push_back(operationVector[i]);
         }
     }
     //обработка финальной строки
-    auto p = name(stack_digit,stack_operation);
-    stack_digit = p.first;
-    stack_operation = p.second;
+    auto p = conversionData(symbolVector,operationVector);
+    symbolVector = p.first;
+    operationVector = p.second;
+    
     //2 раз для ситуации типо 6*5*x*x, иначе он конвертирует в 30*2*x^1, а так в 60*x^1
-    p = name(stack_digit,stack_operation);
-    stack_digit = p.first;
-    stack_operation = p.second;
-    
-//MARK: Convert data from Vector to String
-    string c = "";
-    string xLine = "x";
-    for (int i=0;i<stack_operation.size();i++){
-        if (stack_digit[i] == -1)
-            c += xLine;
-        else
-            c += to_string(stack_digit[i]);
-        c += stack_operation[i]; //в теории тут можно проверку добавить на digite i+1 != числу
-    }
-    if (stack_digit[stack_digit.size()-1] == -1)
-        c += "1";
-    else
-        c += to_string(stack_digit[stack_digit.size()-1]);
-    
-    return c;
+    p = conversionData(symbolVector,operationVector);
+    symbolVector = p.first;
+    operationVector = p.second;
+
+    return conversionDataToString(operationVector, symbolVector);
 }
 
 //MARK:Main
@@ -229,15 +235,20 @@ int main(){
         cout << "Введите выражение: \t";
         cin >> str;
         
-        if (check(str)){
-            cout << "Выражение соответствует грамматике.\n";
-            cout << Ans() << endl;
-        }else{
-            cout << "Некорректный ввод!\n";
+        switch (checkData(str)){
+            case true:{
+                cout << "Выражение соответствует грамматике.\n";
+                cout << separationData() << endl;
+                break;
+            }
+            case false:{
+                cout << "Некорректный ввод!\n";
+                break;
+            }
         }
         
-        stack_digit.clear();
-        stack_operation.clear();
+        symbolVector.clear();
+        operationVector.clear();
         cout << "\n";
     }
     return 0;
