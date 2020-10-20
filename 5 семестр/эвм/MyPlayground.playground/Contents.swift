fecdd2f04e90e9b772d244eee3b9bbce1f779f94
kiraykorIOS <60466904+kiraykorIOS@users.noreply.github.com>
2020-10-20 11:21:39 +0300

    some data

--- 5 семестр/эвм/MyPlayground.playground/Contents.swift
+++ 5 семестр/эвм/MyPlayground.playground/Contents.swift
@@ -1,13 +1,22 @@
-class Solution {
-    
-    struct Point{
-        var x:Int
-        var y:Int
-    }
-    
-    func isPathCrossing(_ path: String) -> Bool {
-        var point = Point(x: 0, y: 0)
-        for i in path{}
-        return true
-    }
+import Foundation
+
+let serialQueue = DispatchQueue(label: "my_serial_queue",attributes: .concurrent)
+
+//Распределить N задач по разным потокам
+DispatchQueue.concurrentPerform(iterations: 10) { (_) in
+    print(Thread.current)
 }
+
+
+//Отслеживать выполнение задачи
+var item = DispatchWorkItem {
+    print("task")
+}
+serialQueue.async(execute: item)
+item.cancel() // можно отменить только пока она не начала выполняться
+//item.notify выполняется, если дже произошла отмена
+
+
+//с помощью Operation можно отменять задачу во время выполнения
+//можно добавить наблюдателя на какое-то свойство(отмена дейстия к примеру)
+
